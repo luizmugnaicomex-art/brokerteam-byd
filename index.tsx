01621195408e933167ba08b02efc7eebcc4778e1
luizmugnaicomex-art <luizmugnai.comex@gmail.com>
2025-10-13 21:24:29 -0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -2998,3 +2998,5 @@ const parseGeneralShipmentFile = (jsonData: any[]): { data: Shipment[], error: s
 const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(<App />);
+
+                
